54d2c0642b727c0f9b5210550c8e12c82b0fc6f8
Vitaly Khabarov <vitkhab@gmail.com>
2017-08-05 16:12:30 +0300

    Added POST processor

--- future-devops/app.py
+++ future-devops/app.py
@@ -1,6 +1,7 @@
 """Little REST service for finding closest email to array of emails.
 """
 import hashlib
+import json
 import falcon
 import distance
 
@@ -16,7 +17,7 @@ class LevensteinResource(object):
         self.res = []
 
     def compare(self):
-        """Compares every email to list of tools. 
+        """Compares every email to list of tools.
         """
         for tool in self.tools:
             tool_hash = hashlib.sha512(tool.encode('utf-8')).hexdigest()
@@ -25,4 +26,17 @@ class LevensteinResource(object):
                 count = distance.levenshtein(tool_hash, email_hash)
                 self.res.append((count, tool, email))
 
+    def on_post(self, req, resp):
+        """Handles GET requests
+        """
+        resp.status = falcon.HTTP_200
+        if req.content_length:
+            data = json.load(req.stream)
+            self.tools = data['tools']
+            self.emails = data['emails']
+            self.compare()
+        resp.body = str(self.res)
+
 api = application = falcon.API()
+
+api.add_route('/levenstein', LevensteinResource())
