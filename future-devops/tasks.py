00976fd0cec2fc7af0289d998c12236ff2259b78
Vitaly Khabarov <vitkhab@gmail.com>
2017-08-09 20:23:24 +0300

    Little optimisation

--- future-devops/tasks.py
+++ future-devops/tasks.py
@@ -13,13 +13,19 @@ def compare(self, tools, emails):
     res = []
     emails_len = len(emails)
     emails_counter = 0
+    tools_hashed = {}
+    for tool in tools:
+        tools_hashed[tool] = hashlib.sha512(tool.encode('utf-8')).hexdigest()
     for email in emails:
         email_hash = hashlib.sha512(email.encode('utf-8')).hexdigest()
         possible_winner = (len(email_hash), email, 'none')
-        for tool in tools:
-            tool_hash = hashlib.sha512(tool.encode('utf-8')).hexdigest()
-            count = distance.levenshtein(tool_hash, email_hash)
-            if count < possible_winner[0]:
+        for tool in tools_hashed:
+            count = distance.levenshtein(
+                tools_hashed[tool],
+                email_hash,
+                max_dist=possible_winner[0]
+            )
+            if 0 <= count < possible_winner[0]:
                 possible_winner = (count, email, tool)
         emails_counter += 1
         self.update_state(state="PROGRESS", meta={'progress': emails_counter/emails_len})
